09cc53ab0009d9741547555db63eaa95d7a9b485
mydongistiny <jaysonedson@gmail.com>
2017-08-25 21:21:42 -0700

    Settings: Add clang version to About Phone
    Change-Id: I3913127d11d15fc9851d6e8a10cad47bf5351c20
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -28,6 +28,7 @@ import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.deviceinfo.AdditionalSystemUpdatePreferenceController;
 import com.android.settings.deviceinfo.BasebandVersionPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
+import com.android.settings.deviceinfo.ClangVersionPreferenceController;
 import com.android.settings.deviceinfo.DeviceModelPreferenceController;
 import com.android.settings.deviceinfo.BuildDatePreferenceController;
 import com.android.settings.deviceinfo.FccEquipmentIdPreferenceController;
@@ -133,6 +134,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new SELinuxStatusPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
+        controllers.add(new ClangVersionPreferenceController(context));
         return controllers;
     }
 
